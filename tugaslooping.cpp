b6aa0544774ac5a071c090614e169993d241c3f6
fer4rie <firapuspita2106@gmail.com>
2026-04-01 14:57:27 +0700

    tambah komentar

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -6,7 +6,7 @@ int angka;
 int pilihan;
 
 // Function untuk memeriksa apakah bilangan n merupakan bilangan prima
-bool cekPrima(int n) { 
+bool cekPrima(int n) { // Bilangan prima adalah bilangan yang hanya memiliki dua faktor, yaitu 1 dan dirinya sendiri
     if (n <= 1) return false;
     int i = 2;
     while (i * i <= n) { 
