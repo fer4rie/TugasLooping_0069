8ae10557f971de24816664d69a00b8ef853d97fc
fer4rie <firapuspita2106@gmail.com>
2026-04-01 14:56:55 +0700

    membuat fungsi cek prima dengan while loop

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -4,3 +4,14 @@ using namespace std;
 // Variabel global
 int angka;
 int pilihan;
+
+// Function untuk memeriksa apakah bilangan n merupakan bilangan prima
+bool cekPrima(int n) { 
+    if (n <= 1) return false;
+    int i = 2;
+    while (i * i <= n) { 
+        if (n % i == 0) return false;
+        i++;
+    }
+    return true;
+}
